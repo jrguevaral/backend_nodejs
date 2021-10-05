b42ad21de4a1da8fbf54ec5566d2f18d5abe9c3b
Javier Guevara <jrguevaral@gmail.com>
2021-10-05 15:00:32 +0200

    Test terminados

--- routes/movies.js
+++ routes/movies.js
@@ -1,68 +1,97 @@
 const express = require('express');
+const MoviesService = require('../services/movies');
+const {
+  movieIdSchema,
+  createMovieSchema,
+  updateMovieSchema
+} = require('../utils/schemas/movies');
+
+const validationHandler = require('../utils/middleware/validationHandler');
 
 function moviesApi(app) {
-    const router = express.Router();
-    app.use("/api/movies", router);
+  const router = express.Router();
+  app.use('/api/movies', router);
 
-    router.get("/", async function(req,res, next){
-        try {
-            const movies = await Promise.resolve(moviesMock)
-            res.status(200).json({
-                data: movies,
-                message: 'movies listed'
-            })
-        }   catch (err) {
-            next(err);
-        }
-    });
+  const moviesService = new MoviesService();
 
-    router.get("/:movieId", async function(req,res, next){
-        try {
-            const movies = await Promise.resolve(moviesMock[0])
-            res.status(200).json({
-                data: movies,
-                message: 'movie retrieved'
-            })
-        }   catch (err) {
-            next(err);
-        }
-    });
+  router.get('/', async function(req, res, next) {
+    const { tags } = req.query;
 
-    router.post("/", async function(req,res, next){
-        try {
-            const createMovieId = await Promise.resolve(moviesMock[0].id)
-            res.status(201).json({
-                data: createMovieId,
-                message: 'movie created'
-            })
-        }   catch (err) {
-            next(err);
-        }
-    });
+    try {
+      const movies = await moviesService.getMovies({ tags });
+      //throw new Error('Error getting movies');
 
-    router.put("/:movieId", async function(req,res, next){
-        try {
-            const updateMovieId = await Promise.resolve(moviesMock[0].id)
-            res.status(200).json({
-                data: updateMovieId,
-                message: 'movies updated'
-            })
-        }   catch (err) {
-            next(err);
-        }
-    });
+      res.status(200).json({
+        data: movies,
+        message: 'movies listed'
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
 
-    router.delete("/:movieId", async function(req,res, next){
-        try {
-            const deleteMovieId = await Promise.resolve(moviesMock[0].id)
-            res.status(200).json({
-                data: deleteMovieId,
-                message: 'movie deleted'
-            })
-        }   catch (err) {
-            next(err);
-        }
-    });
+  router.get('/:movieId', validationHandler({ movieId: movieIdSchema}, 'params'), async function(req, res, next) {
+    const { movieId } = req.params;
+
+    try {
+      const movies = await moviesService.getMovie({ movieId });
+
+      res.status(200).json({
+        data: movies,
+        message: 'movie retrieved'
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.post('/', validationHandler(createMovieSchema), async function(req, res, next) {
+    const { body: movie } = req;
+    try {
+      const createdMovieId = await moviesService.createMovie({ movie });
+
+      res.status(201).json({
+        data: createdMovieId,
+        message: 'movie created'
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.put('/:movieId', validationHandler({ movieId: movieIdSchema}, 'params'), validationHandler(updateMovieSchema), async function(req, res, next) {
+    const { movieId } = req.params;
+    const { body: movie } = req;
+
+    try {
+      const updatedMovieId = await moviesService.updateMovie({
+        movieId,
+        movie
+      });
+
+      res.status(200).json({
+        data: updatedMovieId,
+        message: 'movie updated'
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.delete('/:movieId', validationHandler({ movieId: movieIdSchema}, 'params'), async function(req, res, next) {
+    const { movieId } = req.params;
+
+    try {
+      const deletedMovieId = await moviesService.deleteMovie({ movieId });
+
+      res.status(200).json({
+        data: deletedMovieId,
+        message: 'movie deleted'
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
 }
 
 module.exports = moviesApi;
--- services/movies.js
+++ services/movies.js
@@ -1,30 +1,40 @@
-const { moviesMock } = require('../utils/mocks/movies');
+const MongoLib = require('../lib/mongo');
 
 class MoviesService {
-    async getMovies() {
-        const movies = await Promise.resolve(moviesMock);
-        return movies || [];
-    }
+  constructor() {
+    this.collection = 'movies';
+    this.mongoDB = new MongoLib();
+  }
 
-    async getMovie() {
-        const movies = await Promise.resolve(moviesMock[0]);
-        return movies || {};
-    }
+  async getMovies({ tags }) {
+    const query = tags && { tags: { $in: tags } };
+    const movies = await this.mongoDB.getAll(this.collection, query);
+    return movies || [];
+  }
 
-    async createMovie() {
-        const createMovieId = await Promise.resolve(moviesMock[0].id);
-        return createMovieId;
-    }
+  async getMovie({ movieId }) {
+    const movie = await this.mongoDB.get(this.collection, movieId);
+    return movie || {};
+  }
 
-    async updateMovie() {
-        const updateMovieId = await Promise.resolve(moviesMock[0].id);
-        return updateMovieId;
-    }
+  async createMovie({ movie }) {
+    const createMovieId = await this.mongoDB.create(this.collection, movie);
+    return createMovieId;
+  }
 
-    async deleteMovie() {
-        const deleteMovieId = await Promise.resolve(moviesMock[0].id);
-        return deleteMovieId;
-    }
+  async updateMovie({ movieId, movie } = {}) {
+    const updatedMovieId = await this.mongoDB.update(
+      this.collection,
+      movieId,
+      movie
+    );
+    return updatedMovieId;
+  }
+
+  async deleteMovie({ movieId }) {
+    const deletedMovieId = await this.mongoDB.delete(this.collection, movieId);
+    return deletedMovieId;
+  }
 }
 
 module.exports = MoviesService;
